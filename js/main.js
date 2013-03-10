1e77a3d41d9850e782bf692fdd56cd432a464372
Devin Lauren Elder <laurenelder@fullsail.edu>
2013-03-10 16:18:19 -0400

    JS template done.

--- js/main.js
+++ js/main.js
@@ -2,3 +2,35 @@
 // ASD Term 1303
 // ASD Application
 // 03/05/2013
+
+$("#homepage").on("pageinit", function() {
+
+});
+
+$("#sportCatPage").on("pageinit", function() {
+
+});
+
+$("#cruiserCatPage").on("pageinit", function() {
+
+});
+
+$("#dirtCatPage").on("pageinit", function() {
+
+});
+
+$("#otherCatPage").on("pageinit", function() {
+
+});
+
+$("#addBikePage").on("pageinit", function() {
+
+});
+
+$("#resultsPage").on("pageinit", function() {
+
+});
+
+$("#aboutPage").on("pageinit", function() {
+
+});
