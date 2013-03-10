6846f45259cb781c8fd2cd32167ea67865c20fdd
Devin Lauren Elder <laurenelder@fullsail.edu>
2013-03-10 17:02:43 -0400

    Completed JSON File.

---
+++ js/json.js
@@ -0,0 +1,88 @@
+// Devin "Lauren" Elder
+// ASD Term 1303
+// ASD Application
+// 03/05/2013
+
+var json = {
+	"bike1": {
+		"bType": ["Bike Type:", "Sport Bike"],
+		"bMake": ["Manufacturer:", "Suzuki"],
+		"bModel": ["Model:", "GSXR-1000"],
+		"bYear": ["Year:", "2005"],
+		"bEngine": ["Engine Size (cc):", "1000"],
+		"bPrice": ["Price ($):", "4500"],
+		"bLocation": ["Location:", "Ocala, FL"],
+		"bNumber": ["Contact Number:", "(111)222-3333"],
+		"bOwned": ["New or Pre-Owned:", "Pre-Owned"],
+		"has_lighting": ["Has Aftermarket Lighting:", "Yes"],
+		"has_bars": ["Has Aftermarket Handlebars:", "No"],
+		"has_seats": ["Has Aftermarket Seats:", "No"],
+		"has_body_parts": ["Has Aftermarket Body Parts:", "No"],
+		"has_intake": ["Has Aftermarket Intake:", "Yes"],
+		"has_exhaust": ["Has Aftermarket Exhaust:", "Yes"],
+		"has_wheels": ["Has Aftermarket Wheels:", "No"],
+		"interest": ["Interest:", "95"],
+		"notes": ["Notes:", "Color: Blue and White"]
+	},
+	"bike2": {
+		"bType": ["Bike Type:", "Sport Bike"],
+		"bMake": ["Manufacturer:", "Yamaha"],
+		"bModel": ["Model:", "YZF-R1"],
+		"bYear": ["Year:", "2008"],
+		"bEngine": ["Engine Size (cc):", "1000"],
+		"bPrice": ["Price ($):", "6000"],
+		"bLocation": ["Location:", "Lakeland, FL"],
+		"bNumber": ["Contact Number:", "(111)222-3333"],
+		"bOwned": ["New or Pre-Owned:", "Pre-Owned"],
+		"has_lighting": ["Has Aftermarket Lighting:", "No"],
+		"has_bars": ["Has Aftermarket Handlebars:", "No"],
+		"has_seats": ["Has Aftermarket Seats:", "No"],
+		"has_body_parts": ["Has Aftermarket Body Parts:", "No"],
+		"has_intake": ["Has Aftermarket Intake:", "Yes"],
+		"has_exhaust": ["Has Aftermarket Exhaust:", "Yes"],
+		"has_wheels": ["Has Aftermarket Wheels:", "Yes"],
+		"interest": ["Interest:", "90"],
+		"notes": ["Notes:", "Color: Raven"]
+	},
+	"bike3": {
+		"bType": ["Bike Type:", "Cruiser"],
+		"bMake": ["Manufacturer:", "Yamaha"],
+		"bModel": ["Model:", "V Star Custom"],
+		"bYear": ["Year:", "2007"],
+		"bEngine": ["Engine Size (cc):", "650"],
+		"bPrice": ["Price ($):", "3400"],
+		"bLocation": ["Location:", "Cocoa, FL"],
+		"bNumber": ["Contact Number:", "(111)222-3333"],
+		"bOwned": ["New or Pre-Owned:", "Pre-Owned"],
+		"has_lighting": ["Has Aftermarket Lighting:", "No"],
+		"has_bars": ["Has Aftermarket Handlebars:", "No"],
+		"has_seats": ["Has Aftermarket Seats:", "No"],
+		"has_body_parts": ["Has Aftermarket Body Parts:", "No"],
+		"has_intake": ["Has Aftermarket Intake:", "No"],
+		"has_exhaust": ["Has Aftermarket Exhaust:", "No"],
+		"has_wheels": ["Has Aftermarket Wheels:", "No"],
+		"interest": ["Interest:", "80"],
+		"notes": ["Notes:", "Color: Red"]
+	},
+	"bike4": {
+		"bType": ["Bike Type:", "Cruiser"],
+		"bMake": ["Manufacturer:", "Yamaha"],
+		"bModel": ["Model:", "V Star"],
+		"bYear": ["Year:", "2013"],
+		"bEngine": ["Engine Size (cc):", "250"],
+		"bPrice": ["Price ($):", "4000"],
+		"bLocation": ["Location:", "Lakeland, FL"],
+		"bNumber": ["Contact Number:", "(111)222-3333"],
+		"bOwned": ["New or Pre-Owned:", "New"],
+		"has_lighting": ["Has Aftermarket Lighting:", "No"],
+		"has_bars": ["Has Aftermarket Handlebars:", "No"],
+		"has_seats": ["Has Aftermarket Seats:", "No"],
+		"has_body_parts": ["Has Aftermarket Body Parts:", "No"],
+		"has_intake": ["Has Aftermarket Intake:", "No"],
+		"has_exhaust": ["Has Aftermarket Exhaust:", "No"],
+		"has_wheels": ["Has Aftermarket Wheels:", "No"],
+		"interest": ["Interest:", "50"],
+		"notes": ["Notes:", "Color: Black"]
+	}
+	
+}
